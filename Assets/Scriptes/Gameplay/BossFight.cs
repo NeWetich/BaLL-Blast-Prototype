0a400a6046ff535d8bb1613ef072d524b20d3c36
NeWetich <62741182+NeWetich@users.noreply.github.com>
2022-03-03 12:19:21 +0300

    Prototype v1.5

--- Assets/Scriptes/Gameplay/BossFight.cs
+++ Assets/Scriptes/Gameplay/BossFight.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BossFight : MonoBehaviour
 {
+    public class Random
+    {
+        internal int Next(int v1, int v2)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
     [SerializeField] public Rigidbody2D rb;
     [SerializeField] public int health;
 
+    public int bossBevaivor;
+    [HideInInspector] public Vector2 direction;
+    [HideInInspector] public float speed = 0.01f;
 
     void Start()
     {
-        
+
     }
 
+    void FixedUpdate()
+    {
+        transform.Translate(direction.normalized * speed);
+        if (transform)// transform.position == , transform.rotation ==
+        {
+
+        }
+    }
+
+    public void BossBevaivor()
+    {
+        Random bevaivor = new Random();
+        bossBevaivor = bevaivor.Next(1, 3);
+        if (bossBevaivor == 1)
+        {
+
+        }
+        else if (bossBevaivor == 2)
+        {
+
+        }
+        else if (bossBevaivor == 3)
+        {
+
+        }
+    }
 
     public void TakeDamage(int damage)
     {
